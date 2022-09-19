4a67a2fa9e340dda0a9eb1e4cc2a4405aa89f68c
LuisG <LuisG@DESKTOP-2DLPEDV>
2022-09-19 18:40:25 -0300

    Adicionar arquivos de projeto.

---
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,43 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "The name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Nome")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "The Description is required")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "The price is required")]
+        [Column(TypeName ="decimal(18,2")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Price")]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = "The Stock is required")]
+        [Range(1, 9990)]
+        [DisplayName("Stock")]
+        public int Stock { get; set; }
+        [MaxLength(250)]
+        [DisplayName("Image")]
+        public string Image { get; set; }
+        public Category Category { get; set; }
+        [DisplayName("Category")]
+        public int CategoryId { get; set; }
+    }
+}
---
+++ CleanArchMvc.Application/Services/ProductService.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private IProductRepository _productRepository;
+        private IMapper _mapper;
+
+        public ProductService(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+        public async Task Add(ProductDTO productDTO)
+        {
+            var product = _mapper.Map<Product>(productDTO);
+            await _productRepository.CreateAsync(product);
+        }
+
+        public async Task<ProductDTO> GetById(int? id)
+        {
+            var productEntity  = await _productRepository.GetByIdAsync(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<ProductDTO> GetProductCategoryId(int? id)
+        {
+            var productEntity = await _productRepository.GetProductsIncludeCategoryByIdAsync(id);
+            return _mapper.Map<ProductDTO>(productEntity);
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProducts()
+        {
+            var productEntity = await _productRepository.GetProductsAsync();
+            return _mapper.Map<IEnumerable<ProductDTO>>(productEntity);
+        }
+
+        public async Task Remove(int? id)
+        {
+            var productEntity = _productRepository.GetByIdAsync(id).Result;
+            await _productRepository.RemoveAsync(productEntity);
+        }
+
+        public async Task Update(ProductDTO productDTO)
+        {
+            var product = _mapper.Map<Product>(productDTO);
+            await _productRepository.UpdateAsync(product);
+        }
+    }
+}
